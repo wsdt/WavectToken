32aba18d99ae31e002acf174836549d9ec5554b3
wsdt <kevin.riedl.privat@gmail.com>
2020-12-06 01:02:53 +0100

    build

---
+++ docs/_next/static/kpPjk0Pt-wNDAixt5AQGJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ee4a9b3d6ce67ecfbb9f.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
